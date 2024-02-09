ba156f1e5e1a95726e9c64b4e7f91538eea72d0d
Souji Thenria <mail@souji-thenria.net>
2024-02-09 23:51:05 +0000

    Update code to only use one interrupt

--- main/esp32_ws2812e.c
+++ main/esp32_ws2812e.c
@@ -11,79 +11,119 @@
 #include "driver/touch_pad.h"
 #include "soc/rtc_periph.h"
 
-#define TOUCH_PIN_ON_OFF 5
+#define TOUCH_PIN_POWER 5
+#define TOUCH_PIN_COLOUR 6
 #define LED_PIN 13
 #define LED_NUM 60
 #define TOUCH_THRESH_NO_USE   (0)
 #define TOUCHPAD_FILTER_TOUCH_PERIOD (10)
 
-bool led_status = false;
+typedef struct led_color {
+    int r;
+    int g;
+    int b;
+} led_color;
+
 static led_strip_handle_t led_strip;
+static bool power_status = false;
+static led_color leds[LED_NUM];
 
-void led_start()
+void led_power()
 {
-    led_strip_refresh(led_strip);
-    led_strip_clear(led_strip);
-    for (int i = 0; i < LED_NUM; i++) {
-        led_strip_set_pixel(led_strip, i, 200, 200, 200);
-        led_strip_refresh(led_strip);
-        vTaskDelay(10 / portTICK_PERIOD_MS);
-    }
-    for (int i = 0; i < 3; i++) {
-        for (int i = 0; i < LED_NUM; i++)
-            led_strip_set_pixel(led_strip, i, 10, 10, 10);
-        led_strip_refresh(led_strip);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-        for (int i = 0; i < LED_NUM; i++)
+    power_status = !power_status;
+    if (power_status) {
+        for (int i = 0; i < LED_NUM; i++) {
+            leds[i].r = leds[i].g = leds[i].b = 200;
             led_strip_set_pixel(led_strip, i, 200, 200, 200);
+            led_strip_refresh(led_strip);
+            vTaskDelay(10 / portTICK_PERIOD_MS);
+        }
         led_strip_refresh(led_strip);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+    } else {
+        for (int i = LED_NUM - 1; i >= 0; i--) {
+            leds[i].r = leds[i].g = leds[i].b = 0;
+            led_strip_set_pixel(led_strip, i, 0, 0, 0);
+            led_strip_refresh(led_strip);
+            vTaskDelay(10 / portTICK_PERIOD_MS);
+        }
+        led_strip_clear(led_strip);
+    }
+    touch_pad_intr_enable();
+    vTaskDelete(NULL);
+}
+
+void led_colour_change()
+{
+    if (!power_status)
+        goto end;
+
+    static bool count_down = true;
+    if ((leds[0].r - 10) <= 0) {
+        count_down = false;
+    } else if ((leds[0].r + 10) > 255) {
+        count_down = true;
+    }
+
+    if (count_down) {
+        for (int i = 0; i < LED_NUM; i++) {
+            leds[i].r = leds[i].g = leds[i].b = leds[i].r - 10;
+            led_strip_set_pixel(led_strip, i, leds[i].r, leds[i].g, leds[i].b);
+        }
+    }
+    else {
+        for (int i = 0; i < LED_NUM; i++) {
+            leds[i].r = leds[i].g = leds[i].b = leds[i].r + 10;
+            led_strip_set_pixel(led_strip, i, leds[i].r, leds[i].g, leds[i].b);
+        }
     }
     led_strip_refresh(led_strip);
 
-    // vTaskDelete(NULL);
+end:
+    touch_pad_intr_enable();
+    vTaskDelete(NULL);
 }
 
-void led_stop()
-{
-    led_strip_clear(led_strip);
-    led_strip_refresh(led_strip);
-    // vTaskDelete(NULL);
-}
-
-void interrupt_on(void *_arg)
+void touch_interrupt(void *_arg)
 {
     touch_pad_intr_disable();
+    uint32_t pad_intr = touch_pad_get_status();
+    // set flag to off
+    static uint32_t interrupt_count = (0 << 0);
     touch_pad_clear_status();
-    if (!led_status) {
-        led_status = true;
-        xTaskCreate(&led_start, "led_start", 4096, NULL, 5, NULL);
+
+    /* The touch results into two interrupts, consume the second one */
+    // check if flag is on
+    if (interrupt_count & (1 << 0)) {
+        // toggle flag
+        interrupt_count ^= (1 << 0);
+        touch_pad_intr_enable();
+        return;
     }
-}
+    // set flag (I could also toggle, but no)
+    interrupt_count |= (1 << 0);
 
-void interrupt_off(void *_arg)
-{
-    touch_pad_intr_disable();
-    touch_pad_clear_status();
-    if (led_status) {
-        led_status = false;
-        xTaskCreate(&led_stop, "led_stop", 4096, NULL, 5, NULL);
+    if ((pad_intr >> TOUCH_PIN_POWER) & 0x01) {
+        xTaskCreate(led_power, "led_power", 4096, NULL, 5, NULL);
+    }
+    if ((pad_intr >> TOUCH_PIN_COLOUR) & 0x01) {
+        xTaskCreate(led_colour_change, "led_colour_change", 4096, NULL, 5, NULL);
     }
 }
 
 void touchpad_init()
 {
+    uint16_t touch_value;
     touch_pad_init();
     touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
     touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
-    touch_pad_config(TOUCH_PIN_ON_OFF, TOUCH_THRESH_NO_USE);
+    touch_pad_config(TOUCH_PIN_POWER, TOUCH_THRESH_NO_USE);
+    touch_pad_config(TOUCH_PIN_COLOUR, TOUCH_THRESH_NO_USE);
     touch_pad_filter_start(TOUCHPAD_FILTER_TOUCH_PERIOD);
-
-    uint16_t touch_value;
-    touch_pad_read_filtered(TOUCH_PIN_ON_OFF, &touch_value);
-    touch_pad_set_thresh(TOUCH_PIN_ON_OFF, touch_value * 2 / 3);
-
-    touch_pad_isr_register(interrupt_on, NULL);
+    touch_pad_read_filtered(TOUCH_PIN_POWER, &touch_value);
+    touch_pad_set_thresh(TOUCH_PIN_POWER, touch_value * 2 / 3);
+    touch_pad_read_filtered(TOUCH_PIN_COLOUR, &touch_value);
+    touch_pad_set_thresh(TOUCH_PIN_COLOUR, touch_value * 2 / 3);
+    touch_pad_isr_register(touch_interrupt, NULL);
     touch_pad_intr_enable();
 }
 
@@ -93,11 +133,13 @@ void led_stripe_init()
         .strip_gpio_num = LED_PIN,
         .max_leds = LED_NUM,
     };
-    led_strip_rmt_config_t rmt_config = {
-        .resolution_hz = 10 * 1000 * 1000, // 10MHz
-        .flags.with_dma = false,
+    led_strip_spi_config_t spi_config = {
+        .clk_src = SPI_CLK_SRC_DEFAULT,
+        .flags.with_dma = true,
+        .spi_bus = SPI2_HOST,
     };
-    led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip);
+    led_strip_new_spi_device(&strip_config, &spi_config, &led_strip);
+
     led_strip_clear(led_strip);
 }
 
@@ -105,20 +147,4 @@ void app_main(void)
 {
     touchpad_init();
     led_stripe_init();
-
-    printf("ready");
-    for (;;) {
-        if (led_status) {
-            printf("change hadler to off\n");
-            touch_pad_isr_deregister(interrupt_on, NULL);
-            touch_pad_isr_register(interrupt_off, NULL);
-        } else {
-            printf("change hadler to on\n");
-            touch_pad_isr_deregister(interrupt_off, NULL);
-            touch_pad_isr_register(interrupt_on, NULL);
-        }
-        printf("led_status: %s\n", led_status ? "true" : "false");
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-        touch_pad_intr_enable();
-    }
 }
