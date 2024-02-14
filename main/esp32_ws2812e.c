621f7f903e4347f5c11ffa4202f468608624f7df
Souji Thenria <mail@souji-thenria.net>
2024-02-14 21:15:27 +0000

    fix: improve rotary encoder accuracy

--- main/esp32_ws2812e.c
+++ main/esp32_ws2812e.c
@@ -11,12 +11,18 @@
 #include "driver/touch_pad.h"
 #include "soc/rtc_periph.h"
 
-#define TOUCH_PIN_POWER 5
-#define TOUCH_PIN_COLOUR 6
-#define LED_PIN 13
 #define LED_NUM 60
 #define TOUCH_THRESH_NO_USE   (0)
 #define TOUCHPAD_FILTER_TOUCH_PERIOD (10)
+
+#define PIN_TOUCH TOUCH_PAD_NUM6
+#define PIN_LED_STRIPE GPIO_NUM_13
+#define PIN_R_DT GPIO_NUM_27
+#define PIN_R_CLK GPIO_NUM_26
+#define PIN_G_DT GPIO_NUM_25
+#define PIN_G_CLK GPIO_NUM_33
+#define PIN_B_DT GPIO_NUM_32
+#define PIN_B_CLK GPIO_NUM_35
 
 typedef struct led_color {
     int r;
@@ -52,36 +58,36 @@ void led_power()
     vTaskDelete(NULL);
 }
 
-void led_colour_change()
-{
-    if (!power_status)
-        goto end;
-
-    static bool count_down = true;
-    if ((leds[0].r - 10) <= 0) {
-        count_down = false;
-    } else if ((leds[0].r + 10) > 255) {
-        count_down = true;
-    }
-
-    if (count_down) {
-        for (int i = 0; i < LED_NUM; i++) {
-            leds[i].r = leds[i].g = leds[i].b = leds[i].r - 10;
-            led_strip_set_pixel(led_strip, i, leds[i].r, leds[i].g, leds[i].b);
-        }
-    }
-    else {
-        for (int i = 0; i < LED_NUM; i++) {
-            leds[i].r = leds[i].g = leds[i].b = leds[i].r + 10;
-            led_strip_set_pixel(led_strip, i, leds[i].r, leds[i].g, leds[i].b);
-        }
-    }
-    led_strip_refresh(led_strip);
-
-end:
-    touch_pad_intr_enable();
-    vTaskDelete(NULL);
-}
+// void led_colour_change()
+// {
+//     if (!power_status)
+//         goto end;
+// 
+//     static bool count_down = true;
+//     if ((leds[0].r - 10) <= 0) {
+//         count_down = false;
+//     } else if ((leds[0].r + 10) > 255) {
+//         count_down = true;
+//     }
+// 
+//     if (count_down) {
+//         for (int i = 0; i < LED_NUM; i++) {
+//             leds[i].r = leds[i].g = leds[i].b = leds[i].r - 10;
+//             led_strip_set_pixel(led_strip, i, leds[i].r, leds[i].g, leds[i].b);
+//         }
+//     }
+//     else {
+//         for (int i = 0; i < LED_NUM; i++) {
+//             leds[i].r = leds[i].g = leds[i].b = leds[i].r + 10;
+//             led_strip_set_pixel(led_strip, i, leds[i].r, leds[i].g, leds[i].b);
+//         }
+//     }
+//     led_strip_refresh(led_strip);
+// 
+// end:
+//     touch_pad_intr_enable();
+//     vTaskDelete(NULL);
+// }
 
 void touch_interrupt(void *_arg)
 {
@@ -102,27 +108,25 @@ void touch_interrupt(void *_arg)
     // set flag (I could also toggle, but no)
     interrupt_count |= (1 << 0);
 
-    if ((pad_intr >> TOUCH_PIN_POWER) & 0x01) {
+    if ((pad_intr >> PIN_TOUCH) & 0x01) {
         xTaskCreate(led_power, "led_power", 4096, NULL, 5, NULL);
     }
-    if ((pad_intr >> TOUCH_PIN_COLOUR) & 0x01) {
-        xTaskCreate(led_colour_change, "led_colour_change", 4096, NULL, 5, NULL);
-    }
+    // if ((pad_intr >> TOUCH_PIN_COLOUR) & 0x01) {
+    //     xTaskCreate(led_colour_change, "led_colour_change", 4096, NULL, 5, NULL);
+    // }
 }
 
 void touchpad_init()
 {
     uint16_t touch_value;
     touch_pad_init();
+
     touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
     touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
-    touch_pad_config(TOUCH_PIN_POWER, TOUCH_THRESH_NO_USE);
-    touch_pad_config(TOUCH_PIN_COLOUR, TOUCH_THRESH_NO_USE);
+    touch_pad_config(PIN_TOUCH, TOUCH_THRESH_NO_USE);
     touch_pad_filter_start(TOUCHPAD_FILTER_TOUCH_PERIOD);
-    touch_pad_read_filtered(TOUCH_PIN_POWER, &touch_value);
-    touch_pad_set_thresh(TOUCH_PIN_POWER, touch_value * 2 / 3);
-    touch_pad_read_filtered(TOUCH_PIN_COLOUR, &touch_value);
-    touch_pad_set_thresh(TOUCH_PIN_COLOUR, touch_value * 2 / 3);
+    touch_pad_read_filtered(PIN_TOUCH, &touch_value);
+    touch_pad_set_thresh(PIN_TOUCH, touch_value * 2 / 3);
     touch_pad_isr_register(touch_interrupt, NULL);
     touch_pad_intr_enable();
 }
@@ -130,7 +134,7 @@ void touchpad_init()
 void led_stripe_init()
 {
     led_strip_config_t strip_config = {
-        .strip_gpio_num = LED_PIN,
+        .strip_gpio_num = PIN_LED_STRIPE,
         .max_leds = LED_NUM,
     };
     led_strip_spi_config_t spi_config = {
@@ -143,8 +147,119 @@ void led_stripe_init()
     led_strip_clear(led_strip);
 }
 
+bool dt = false;
+void rotary_colour(void *_arg)
+{
+    int v = *((int *) _arg);
+
+
+    if (dt) {
+        switch (v) {
+            case PIN_R_CLK:
+                for (int i = 0; i < LED_NUM; i++) {
+                    leds[i].r -= 10;
+                    led_strip_set_pixel(led_strip, i, leds[i].r, leds[i].g, leds[i].b);
+                }
+                break;
+            case PIN_G_CLK:
+                for (int i = 0; i < LED_NUM; i++) {
+                    leds[i].g -= 10;
+                    led_strip_set_pixel(led_strip, i, leds[i].r, leds[i].g, leds[i].b);
+                }
+                break;
+            case PIN_B_CLK:
+                for (int i = 0; i < LED_NUM; i++) {
+                    leds[i].b -= 10;
+                    led_strip_set_pixel(led_strip, i, leds[i].r, leds[i].g, leds[i].b);
+                }
+                break;
+        }
+    } else {
+        switch (v) {
+            case PIN_R_CLK:
+                for (int i = 0; i < LED_NUM; i++) {
+                    leds[i].r += 10;
+                    led_strip_set_pixel(led_strip, i, leds[i].r, leds[i].g, leds[i].b);
+                }
+                break;
+            case PIN_G_CLK:
+                for (int i = 0; i < LED_NUM; i++) {
+                    leds[i].g += 10;
+                    led_strip_set_pixel(led_strip, i, leds[i].r, leds[i].g, leds[i].b);
+                }
+                break;
+            case PIN_B_CLK:
+                for (int i = 0; i < LED_NUM; i++) {
+                    leds[i].b += 10;
+                    led_strip_set_pixel(led_strip, i, leds[i].r, leds[i].g, leds[i].b);
+                }
+                break;
+        }
+    }
+    led_strip_refresh(led_strip);
+
+    printf("(%d, %d, %d)\n", leds[0].r,leds[0].g,leds[0].b);
+
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+    dt = false;
+    gpio_intr_enable(v);
+    gpio_intr_enable(PIN_R_DT);
+    gpio_intr_enable(PIN_G_DT);
+    gpio_intr_enable(PIN_B_DT);
+    vTaskDelete(NULL);
+}
+
+void rotary_interrupt(void *_arg)
+{
+    int p = *((int *)_arg);
+
+    gpio_intr_disable(p);
+    switch (p) {
+        case PIN_R_CLK:
+            gpio_intr_disable(PIN_R_DT);
+            xTaskCreate(rotary_colour, "rotary_colour", 4096, _arg, 5, NULL);
+            break;
+        case PIN_G_CLK:
+            gpio_intr_disable(PIN_G_DT);
+            xTaskCreate(rotary_colour, "rotary_colour", 4096, _arg, 5, NULL);
+            break;
+        case PIN_B_CLK:
+            gpio_intr_disable(PIN_B_DT);
+            xTaskCreate(rotary_colour, "rotary_colour", 4096, _arg, 5, NULL);
+            break;
+        case PIN_R_DT: case PIN_G_DT: case PIN_B_DT:
+            dt = true;
+            break;
+    }
+}
+
+gpio_num_t pins[] = {
+    PIN_R_CLK,
+    PIN_G_CLK,
+    PIN_B_CLK,
+    PIN_R_DT,
+    PIN_G_DT,
+    PIN_B_DT,
+};
+
+void rotary_init()
+{
+    size_t pins_arr_len = 6;
+    int i;
+
+    gpio_install_isr_service(0);
+
+    for (i = 0; i < pins_arr_len; i++) {
+        gpio_set_direction(pins[i], GPIO_MODE_INPUT);
+        gpio_set_intr_type(pins[i], GPIO_INTR_NEGEDGE);
+        gpio_intr_enable(pins[i]);
+        gpio_isr_handler_add(pins[i], rotary_interrupt, &pins[i]);
+    }
+}
+
 void app_main(void)
 {
     touchpad_init();
     led_stripe_init();
+    rotary_init();
 }
