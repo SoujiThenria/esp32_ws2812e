43d8789bc495bb99a4b2769bd871e45267a1a562
Souji Thenria <mail@souji-thenria.net>
2024-02-16 02:35:25 +0000

    Improve code

--- main/esp32_ws2812e.c
+++ main/esp32_ws2812e.c
@@ -31,6 +31,9 @@
 #define PIN_B_DT GPIO_NUM_32
 #define PIN_B_CLK GPIO_NUM_35
 
+#define PIN_BRIGHTNESS_DT GPIO_NUM_34
+#define PIN_BRIGHTNESS_CLK GPIO_NUM_39
+
 /* It might be a good idea to split the colours in to different arrays and use
  * memset for better performance. */
 typedef struct led_color {
@@ -43,13 +46,11 @@ static led_strip_handle_t led_strip;
 static led_color led[LED_NUM];
 static volatile bool led_power_status;
 static volatile bool dt = false;
-gpio_num_t pins[] = {
-    PIN_R_CLK,
-    PIN_G_CLK,
-    PIN_B_CLK,
-    PIN_R_DT,
-    PIN_G_DT,
-    PIN_B_DT,
+gpio_num_t pins[][2] = {
+    { PIN_R_CLK, PIN_R_DT },
+    { PIN_G_CLK, PIN_G_DT },
+    { PIN_B_CLK, PIN_B_DT },
+    { PIN_BRIGHTNESS_CLK, PIN_BRIGHTNESS_DT },
 };
 
 void led_power()
@@ -91,29 +92,46 @@ void touch_interrupt(void *_arg)
     xTaskCreate(led_power, "led_power", 4096, NULL, 5, NULL);
 }
 
+void led_update()
+{
+    int r,g,b;
+    for (int i = 0; i < LED_NUM; i++) {
+        r = led[i].r > 0 ? led[i].r : 0;
+        g = led[i].g > 0 ? led[i].g : 0;
+        b = led[i].b > 0 ? led[i].b : 0;
+        ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, r, g, b));
+    }
+    ESP_ERROR_CHECK(led_strip_refresh(led_strip));
+    printf("(%d, %d, %d)\n", led[0].r,led[0].g,led[0].b);
+}
+
 void rotary_colour(void *_arg)
 {
     int v = *((int *) _arg);
-
+    int vv = ((int *) _arg)[1];
 
     if (dt) {
         switch (v) {
             case PIN_R_CLK:
                 for (int i = 0; i < LED_NUM; i++) {
-                    led[i].r -= 10;
-                    led_strip_set_pixel(led_strip, i, led[i].r, led[i].g, led[i].b);
+                    led[i].r -= 5;
                 }
                 break;
             case PIN_G_CLK:
                 for (int i = 0; i < LED_NUM; i++) {
-                    led[i].g -= 10;
-                    led_strip_set_pixel(led_strip, i, led[i].r, led[i].g, led[i].b);
+                    led[i].g -= 5;
                 }
                 break;
             case PIN_B_CLK:
                 for (int i = 0; i < LED_NUM; i++) {
-                    led[i].b -= 10;
-                    led_strip_set_pixel(led_strip, i, led[i].r, led[i].g, led[i].b);
+                    led[i].b -= 5;
+                }
+                break;
+            case PIN_BRIGHTNESS_CLK:
+                for (int i = 0; i < LED_NUM; i++) {
+                    led[i].r -= 1;
+                    led[i].g -= 1;
+                    led[i].b -= 1;
                 }
                 break;
         }
@@ -121,34 +139,34 @@ void rotary_colour(void *_arg)
         switch (v) {
             case PIN_R_CLK:
                 for (int i = 0; i < LED_NUM; i++) {
-                    led[i].r += 10;
-                    led_strip_set_pixel(led_strip, i, led[i].r, led[i].g, led[i].b);
+                    led[i].r += 5;
                 }
                 break;
             case PIN_G_CLK:
                 for (int i = 0; i < LED_NUM; i++) {
-                    led[i].g += 10;
-                    led_strip_set_pixel(led_strip, i, led[i].r, led[i].g, led[i].b);
+                    led[i].g += 5;
                 }
                 break;
             case PIN_B_CLK:
                 for (int i = 0; i < LED_NUM; i++) {
-                    led[i].b += 10;
-                    led_strip_set_pixel(led_strip, i, led[i].r, led[i].g, led[i].b);
+                    led[i].b += 5;
+                }
+                break;
+            case PIN_BRIGHTNESS_CLK:
+                for (int i = 0; i < LED_NUM; i++) {
+                    led[i].r += 1;
+                    led[i].g += 1;
+                    led[i].b += 1;
                 }
                 break;
         }
     }
-    led_strip_refresh(led_strip);
+    led_update();
 
-    printf("(%d, %d, %d)\n", led[0].r,led[0].g,led[0].b);
-
-    vTaskDelay(50 / portTICK_PERIOD_MS);
+    vTaskDelay(80 / portTICK_PERIOD_MS);
     dt = false;
-    gpio_intr_enable(v);
-    gpio_intr_enable(PIN_R_DT);
-    gpio_intr_enable(PIN_G_DT);
-    gpio_intr_enable(PIN_B_DT);
+    ESP_ERROR_CHECK(gpio_intr_enable(v));
+    ESP_ERROR_CHECK(gpio_intr_enable(vv));
     vTaskDelete(NULL);
 }
 
@@ -156,21 +174,25 @@ void rotary_interrupt(void *_arg)
 {
     int p = *((int *)_arg);
 
-    gpio_intr_disable(p);
+    ESP_ERROR_CHECK(gpio_intr_disable(p));
     switch (p) {
         case PIN_R_CLK:
-            gpio_intr_disable(PIN_R_DT);
+            ESP_ERROR_CHECK(gpio_intr_disable(PIN_R_DT));
             xTaskCreate(rotary_colour, "rotary_colour", 4096, _arg, 5, NULL);
             break;
         case PIN_G_CLK:
-            gpio_intr_disable(PIN_G_DT);
+            ESP_ERROR_CHECK(gpio_intr_disable(PIN_G_DT));
             xTaskCreate(rotary_colour, "rotary_colour", 4096, _arg, 5, NULL);
             break;
         case PIN_B_CLK:
-            gpio_intr_disable(PIN_B_DT);
+            ESP_ERROR_CHECK(gpio_intr_disable(PIN_B_DT));
             xTaskCreate(rotary_colour, "rotary_colour", 4096, _arg, 5, NULL);
             break;
-        case PIN_R_DT: case PIN_G_DT: case PIN_B_DT:
+        case PIN_BRIGHTNESS_CLK:
+            ESP_ERROR_CHECK(gpio_intr_disable(PIN_BRIGHTNESS_DT));
+            xTaskCreate(rotary_colour, "rotary_colour", 4096, _arg, 5, NULL);
+            break;
+        default:
             dt = true;
             break;
     }
@@ -209,16 +231,18 @@ void led_stripe_init()
 
 void rotary_init()
 {
-    size_t pins_arr_len = 6;
-    int i;
+    size_t pins_arr_len = 4;
+    int i,j;
 
     ESP_ERROR_CHECK(gpio_install_isr_service(0));
 
     for (i = 0; i < pins_arr_len; i++) {
-        ESP_ERROR_CHECK(gpio_set_direction(pins[i], GPIO_MODE_INPUT));
-        ESP_ERROR_CHECK(gpio_set_intr_type(pins[i], GPIO_INTR_NEGEDGE));
-        ESP_ERROR_CHECK(gpio_intr_enable(pins[i]));
-        ESP_ERROR_CHECK(gpio_isr_handler_add(pins[i], rotary_interrupt, &pins[i]));
+        for (j = 0; j < 2; j++) {
+            ESP_ERROR_CHECK(gpio_set_direction(pins[i][j], GPIO_MODE_INPUT));
+            ESP_ERROR_CHECK(gpio_set_intr_type(pins[i][j], GPIO_INTR_NEGEDGE));
+            ESP_ERROR_CHECK(gpio_intr_enable(pins[i][j]));
+            ESP_ERROR_CHECK(gpio_isr_handler_add(pins[i][j], rotary_interrupt, &pins[i][j]));
+        }
     }
 }
 
